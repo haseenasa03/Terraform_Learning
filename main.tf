28e818fa4d3b78020575a7b12db561d8b3232291
Haseena S.A <113978095+haseenasa03@users.noreply.github.com>
2024-08-07 00:30:33 +0530

    Create main.tf
    terraform ec2-instance config

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-04a81a99f5ec58529"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+    key_name = "ec2-terraform"
+    
+}
